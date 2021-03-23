5de147bf24707ec71331ffdbffeb22e5bf44b927
gongzhou.yang@appmagics.com <gongzhou.yang@appmagics.com>
2021-03-23 21:28:53 +0800

    +1

--- src/components/editor/common/word_index.js
+++ src/components/editor/common/word_index.js
@@ -1,4 +1,3 @@
-import { CmuServer } from "./cmu";
 
 export class ComputerWords {
 
@@ -19,31 +18,5 @@ export class ComputerWords {
   getIndex_ZH(str) {
     return str.replace(/[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?|\r\n]/g,"")
       .match(/[\u4e00-\u9fa5_0-9]/g)
-  }
-
-  //英文——————————计算词汇，处理混杂的每个数字和汉字前后增加空格----biaobei
-  getIndex_EN_BB(str,cmuInfo,intervalInfo) {
-    if(cmuInfo && intervalInfo){
-      this.cmuInfo = cmuInfo;
-      this.intervalInfo = intervalInfo;
-    }
-    // console.log('str',str)
-    if(!str || !str.match(/[a-zA-Z]+/ig)){
-      return 0
-    }
-    let wordsLength  = str.match(/[a-zA-Z]+/ig).length
-    let wordsCmu =  this.cmuInfo.slice(0,wordsLength)
-    let cmuLength = 0;
-    wordsCmu.forEach(word=>{
-      if(word){
-        cmuLength += word.split(' ').length
-      }else {
-        //没有能够转换的词汇，例如admin 返回音标是null or undefined
-        //--等需求处理 估算或者cmu自定义
-
-      }
-
-    })
-    return cmuLength
   }
 }
